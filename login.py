117e8b9a0cdec3bb83636d63a97ff749b4170148
hzxjy <hellagur4225@gmail.com>
2022-05-09 23:38:33 +0800

    init

---
+++ login.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import sys
+
+cookies = {
+    'JSESSIONID': sys.argv[1],
+}
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Origin': 'http://portal.gd165.com',
+    'Referer': 'http://portal.gd165.com/index.do',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',
+}
+
+data = [
+    ('loginpage', 'gd/blg/login.jsp'),
+    ('onlinepage', 'gd/blg/online.jsp'),
+    ('logoutpage', ''),
+    ('accountprefixname', ''),
+    ('accountsuffixname', ''),
+    ('pagetype', '0'),
+    ('macauth', '0'),
+    ('accountvalid', '1800'),
+    ('customerId', '001'),
+    ('customerName', 'campus'),
+    ('basName', '120.80.200.50'),
+    ('basPushUrl', 'http://portal.gd165.com/?wlanuserip=10.130.128.135&wlanacname=&basname=120.80.200.50&ssid=bitzh.edu&vlanid=ethtrunk/10:4000.0'),
+    ('accountName', ''),
+    ('sendSMS', ''),
+    ('attrName', 'ssid'),
+    ('attrValue', '[bitzh.edu]'),
+    ('realmName', ''),
+    ('fixedAccountPrefixName', ''),
+    ('errormessage', ''),
+    ('keepAliveTime', ''),
+    ('wlanuserip', '10.130.128.135'),
+    ('client_type', 'pz'),
+    ('basname', '120.80.200.50'),
+    ('errormessage', ''),
+    ('setUserOnline', ''),
+    ('userOpenAddress', ''),
+    ('accountType', ''),
+    ('token', sys.argv[2]),
+    ('username', ''),
+    ('password', ''),
+]
+
+response = requests.post('http://portal.gd165.com/login.do', cookies=cookies, headers=headers, data=data, verify=False)
+print(response.text)
---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from byod import byod
+
+def main():
+    if(byod()):
+        print("byod connection successfully")
+
+if __name__=='__main__':
+    main()
