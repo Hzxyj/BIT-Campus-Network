e7deb370d315d044b4be442689adac6528fddde4
hzxjy <hellagur4225@gmail.com>
2022-05-10 16:44:43 +0800

    edit main

--- main.py
+++ main.py
@@ -2,10 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from byod import byod
+import json
+import sys
 
-def main():
-    if(byod()):
-        print("byod connection successfully")
+class auto_load:
+    __config=None
+
+    def __init__(self,json):
+        self.__set_config(json)
+
+    def __set_config(self,conf):
+        try:
+            with open(conf,'r') as js:
+                self.__config=json.load(js)
+            print("aaaa")
+        except:
+
+    def main(self):
+        if(byod(self.__config['id'],self.__config['byodpw'])):
+            print("byod connection successfull")
+        else:
+            print("unexpected error in byod")
 
 if __name__=='__main__':
-    main()
+    load=auto_load("loginf.json")
+    load.main()
